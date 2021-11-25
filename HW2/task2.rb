c7918bca1bd539001076eaa4c20c80fbe2c3bccf
IevgenGr <94597794+IevgenGr@users.noreply.github.com>
2021-11-25 18:10:27 +0200

    Create task2.rb

---
+++ HW2/task2.rb
@@ -0,0 +1,14 @@
+class Dog
+  
+ def initialize name
+    @name = name
+    @asleep = false
+    @stuffInBelly     = 10  #  Он сыт.
+    @stuffInIntestine =  0  #  Ему не надо гулять.
+    
+    puts @name + ' родился.'
+  end  
+  
+end
+
+
